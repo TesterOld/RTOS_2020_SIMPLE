d16b2387064b426e88414610f5210b15467eb590
shok09 <shok09-1@mail.ru>
2020-05-28 09:23:33 +0300

    LAB 3 Update
    Pankiv O. V.

--- LAB_3_2_1_int_step/lib/sysDelay.c
+++ LAB_3_2_1_int_step/lib/sysDelay.c
@@ -1,26 +1,8 @@
 #include "sysDelay.h"
 
-void delay_ms( uint32_t Val) 
-{
-	Val=Val*10000;
-	for( ; Val != 0; Val--) 
-  		{
-		__nop();
-  		}
-}
-
-void btn_init_in_c15 (void){
-    
-   
-    GPIO_InitTypeDef GPIO_InitStructure;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC , ENABLE); // (1)
-    
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;    
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
+int substep_num = 0;
+int speed = 1000 ;
+bool clockwise = 1;
 
 
 //static __IO uint32_t TimingDelay;
